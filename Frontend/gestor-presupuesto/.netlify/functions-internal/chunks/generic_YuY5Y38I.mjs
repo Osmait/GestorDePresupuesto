69974639eab5f3186f6f8d108281402827fe8f87
osmait <saulburgos6@gmail.com>
2023-12-14 17:45:18 -0400

    fix deploy

---
+++ Frontend/gestor-presupuesto/.netlify/functions-internal/chunks/generic_YuY5Y38I.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_Enk22OdP.mjs');
+
+export { page };
