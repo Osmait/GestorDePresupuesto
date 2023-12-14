998624bbb6717fbac933fbf032befbc7be77c491
osmait <saulburgos6@gmail.com>
2023-12-14 19:56:50 -0400

    deploy

--- Frontend/gestor-presupuesto/.netlify/functions-internal/chunks/accounts_AHLDP_Tb.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/accounts_9ZazTy6A.mjs').then(n => n.a);
-
-export { page };
--- Frontend/gestor-presupuesto/.netlify/functions-internal/chunks/transactions_WlR_FQVH.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/transactions_6HlJyNWm.mjs');
-
-export { page };
