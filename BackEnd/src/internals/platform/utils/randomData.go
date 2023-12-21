a1ddd7b1adbee43e0533dd6563944b289f101f27
osmait <saulburgos6@gmail.com>
2023-12-21 15:28:27 -0400

    refactor name to randomData

--- BackEnd/src/internals/platform/utils/randomData.go
+++ BackEnd/src/internals/platform/utils/randomData.go
@@ -9,20 +9,20 @@ import (
 	"github.com/osmait/gestorDePresupuesto/src/internals/domain/user"
 )
 
-func GetNewUser() *user.User {
-	user1 := user.NewUser(faker.ID, faker.Name(), faker.LastName(), faker.Email(), faker.Password())
+func GetNewRandomUser() *user.User {
+	user1 := user.NewUser(faker.UUIDDigit(), faker.Name(), faker.LastName(), faker.Email(), faker.Password())
 	return user1
 }
 
 func GetNewRandomAccount() *account.Account {
 	balances, _ := faker.RandomInt(1, 10000000)
 
-	return account.NewAccount(float64(balances[0]), faker.ID, faker.Name(), faker.Name())
+	return account.NewAccount(float64(balances[0]), faker.UUIDDigit(), faker.Name(), faker.Name())
 }
 
 func GetNewRandomTransaction() *transaction.Transaction {
 	trasanctionType := []string{"bill", "income"}
 	balances, _ := faker.RandomInt(1, 10000000)
 
-	return transaction.NewTransaction(faker.ID, faker.Name(), faker.Paragraph(), trasanctionType[rand.Intn(2)], faker.ID, float64(balances[0]))
+	return transaction.NewTransaction(faker.UUIDDigit(), faker.Name(), faker.Paragraph(), trasanctionType[rand.Intn(2)], faker.UUIDDigit(), float64(balances[0]))
 }
