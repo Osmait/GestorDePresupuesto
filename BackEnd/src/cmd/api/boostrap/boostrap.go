04b2bfe4a455308c5d8bde8dfc38c8de4b18dfa3
osmait <saulburgos6@gmail.com>
2023-12-21 15:26:04 -0400

    extract config logic to config packge

--- BackEnd/src/cmd/api/boostrap/boostrap.go
+++ BackEnd/src/cmd/api/boostrap/boostrap.go
@@ -8,18 +8,18 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+	"github.com/osmait/gestorDePresupuesto/src/config"
 	"github.com/osmait/gestorDePresupuesto/src/internals/platform/server"
 	"github.com/osmait/gestorDePresupuesto/src/internals/platform/storage/postgress"
+	"github.com/osmait/gestorDePresupuesto/src/internals/platform/utils"
 	"github.com/osmait/gestorDePresupuesto/src/internals/services/account"
 	"github.com/osmait/gestorDePresupuesto/src/internals/services/auth"
 	"github.com/osmait/gestorDePresupuesto/src/internals/services/transaction"
 	"github.com/osmait/gestorDePresupuesto/src/internals/services/user"
-	"github.com/rs/zerolog/log"
 )
 
 func Run() error {
@@ -27,68 +27,28 @@ func Run() error {
 	if err != nil {
 		fmt.Println("Not env")
 	}
-	var cfg Config
 	shutdown := 10 * time.Second
-
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
-	cfg.shutdownTimeout = &shutdown
 	dbPort, _ := strconv.Atoi(os.Getenv("DbPort"))
-	cfg.Port = uint(port)
-	cfg.Host = os.Getenv("HOST")
+	cfg := config.NewConfig(os.Getenv("HOST"), uint(port), uint(dbPort), &shutdown, os.Getenv("DbUser"), os.Getenv("DbPass"), os.Getenv("Dbhost"), os.Getenv("DbName"))
 
-	cfg.DbName = os.Getenv("DbName")
-	cfg.DbUser = os.Getenv("DbUser")
-	cfg.DbPass = os.Getenv("DbPass")
-	cfg.DbPort = uint(dbPort)
-	cfg.Dbhost = os.Getenv("Dbhost")
-
-	postgresURI := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", cfg.DbUser, cfg.DbPass, cfg.Dbhost, cfg.DbPort, cfg.DbName)
+	postgresURI := cfg.GetPostgresUrl()
 
 	db, err := sql.Open("postgres", postgresURI)
 	if err != nil {
 		return err
 	}
-	runDBMigration("file://src/cmd/api/db/migrations", postgresURI)
+	utils.RunDBMigration("file://src/cmd/api/db/migrations", postgresURI)
 
-	accountRepository := postgress.NewCourseRepository(db)
+	accountRepository := postgress.NewAccountRepository(db)
 	transactionRepository := postgress.NewTransactionRepository(db)
 	userRepository := postgress.NewUserRespository(db)
 	accountSerevice := account.NewAccountService(accountRepository)
 	transactionServices := transaction.NewTransactionService(transactionRepository)
 	userServices := user.NewUserService(userRepository)
 	authServices := auth.NewAuthService(userRepository)
-	ctx, srv := server.New(context.Background(), cfg.Host, cfg.Port, cfg.shutdownTimeout, accountSerevice, transactionServices, userServices, authServices)
+
+	ctx, srv := server.New(context.Background(), cfg.Host, cfg.Port, cfg.ShutdownTimeout, accountSerevice, transactionServices, userServices, authServices)
+
 	return srv.Run(ctx)
-}
-
-func runDBMigration(migrationURL string, dbSource string) {
-	fmt.Println(migrationURL)
-	migration, err := migrate.New(migrationURL, dbSource)
-	if err != nil {
-		log.Fatal().Err(err).Msg("cannot create new migrate instance")
-	}
-
-	// if err = migration.Force(2); err != nil {
-	// 	log.Fatal().Err(err)
-	// }
-	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal().Err(err).Msg("failed to run migrate up")
-	}
-
-	log.Info().Msg("db migrated successfully")
-}
-
-type Config struct {
-	// Server Configuration
-	Host            string
-	Port            uint
-	shutdownTimeout *time.Duration
-
-	// DataBase Configuration
-
-	DbUser string
-	DbPass string
-	Dbhost string
-	DbPort uint
-	DbName string
 }
