0899c1d45485da9473835c7e29a9b9dc8ed47a4a
osmait <saulburgos6@gmail.com>
2023-12-23 19:32:53 -0400

    chanes accounts names to transaction

--- BackEnd/src/internals/services/transaction/transactionService.go
+++ BackEnd/src/internals/services/transaction/transactionService.go
@@ -42,12 +42,12 @@ func (s TransactionService) CreateTransaction(ctx context.Context, name, descrpi
 }
 
 func (s TransactionService) FindAll(ctx context.Context, date string, date2 string, id string) ([]*transaction.Transaction, error) {
-	accounts, err := s.transactionRepository.FindAll(ctx, date, date2, id)
+	transaction, err := s.transactionRepository.FindAll(ctx, date, date2, id)
 	if err != nil {
 		return nil, err
 	}
 
-	return accounts, nil
+	return transaction, nil
 }
 
 func (s TransactionService) FindAllOfAllAccounts(ctx context.Context, date string, date2 string, id string) ([]*transaction.Transaction, error) {
