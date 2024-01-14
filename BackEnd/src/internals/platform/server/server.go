57ca87fe83402f6b5a0116f160cf751e73589465
osmait <saulburgos6@gmail.com>
2024-01-14 17:35:05 -0400

    fix cors

--- BackEnd/src/internals/platform/server/server.go
+++ BackEnd/src/internals/platform/server/server.go
@@ -45,9 +45,10 @@ func New(ctx context.Context, host string, port uint, shutdownTimeout *time.Dura
 }
 
 func (s *Server) registerRoutes() {
+	s.Engine.Use(cors.AllowAll())
 	s.Engine.Use(middleware.AuthMiddleware(s.servicesUser))
 	routes.HealthRoutes(s.Engine)
-	s.Engine.Use(cors.AllowAll())
+
 	routes.AuhtRoutes(s.Engine, s.servicesAuth)
 	routes.UserRoute(s.Engine, s.servicesUser)
 	routes.AccountRotes(s.Engine, s.servicesAccunt)
