801c0755e5ca660a7c221da3e16f23908c70f25e
osmait <saulburgos6@gmail.com>
2024-01-15 16:24:09 -0400

    add userID field

--- BackEnd/src/internals/domain/invesment/invesment.go
+++ BackEnd/src/internals/domain/invesment/invesment.go
@@ -6,17 +6,19 @@ type Invesment struct {
 	CreatedAt    time.Time
 	Id           string
 	Name         string
+	UserId       string
 	Price        float64
 	CurrentPrice float64
-	Quantity     int
+	Quantity     float64
 }
 
-func NewInvesment(id, name string, price, currentPrice float64, quantity int) *Invesment {
+func NewInvesment(id, name string, price, currentPrice float64, quantity float64, userId string) *Invesment {
 	return &Invesment{
 		Id:           id,
 		Name:         name,
 		Price:        price,
 		CurrentPrice: currentPrice,
 		Quantity:     quantity,
+		UserId:       userId,
 	}
 }
