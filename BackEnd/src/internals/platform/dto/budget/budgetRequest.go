d708134c6166c45273443dea765830715bea8fd3
osmait <saulburgos6@gmail.com>
2024-01-17 19:31:20 -0400

    budget dto test

--- BackEnd/src/internals/platform/dto/budget/budgetRequest.go
+++ BackEnd/src/internals/platform/dto/budget/budgetRequest.go
@@ -1,15 +1,13 @@
 package dto
 
 type BudgetRequest struct {
-	CategoryId    string  `json:"category_id"`
-	Amount        float64 `json:"amount"`
-	CurrentAmount float64 `json:"current_amount"`
+	CategoryId string  `json:"category_id"`
+	Amount     float64 `json:"amount"`
 }
 
-func NewBudgetRequest(categoryId string, amount, curentAmount float64) *BudgetRequest {
+func NewBudgetRequest(categoryId string, amount float64) *BudgetRequest {
 	return &BudgetRequest{
-		CategoryId:    categoryId,
-		Amount:        amount,
-		CurrentAmount: curentAmount,
+		CategoryId: categoryId,
+		Amount:     amount,
 	}
 }
