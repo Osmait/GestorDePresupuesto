32cdca4497b0edc0f5be8d6da5674272ea03066a
osmait <saulburgos6@gmail.com>
2024-01-17 15:55:53 -0400

    created user dto

---
+++ BackEnd/src/internals/platform/dto/user/userRequest.go
@@ -0,0 +1,17 @@
+package dto
+
+type UserRequest struct {
+	Name     string `json:"name"`
+	LastName string `json:"last_name"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+func NewUserRequest(id, name, lastName, password, email string) *UserRequest {
+	return &UserRequest{
+		Name:     name,
+		LastName: lastName,
+		Email:    email,
+		Password: password,
+	}
+}
