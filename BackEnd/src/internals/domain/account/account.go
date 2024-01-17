934675538bb18670c4b7b735dc0ad7a7e3640711
osmait <saulburgos6@gmail.com>
2024-01-17 15:55:17 -0400

    created account dto

--- BackEnd/src/internals/domain/account/account.go
+++ BackEnd/src/internals/domain/account/account.go
@@ -1,11 +1,14 @@
 package account
 
+import "time"
+
 type Account struct {
-	Id             string  `json:"id"`
-	Name           string  `json:"name"`
-	Bank           string  `json:"bank"`
-	UserId         string  `json:"user_id"`
-	InitialBalance float64 `json:"initialBalance"`
+	Id             string    `json:"id"`
+	Name           string    `json:"name"`
+	Bank           string    `json:"bank"`
+	UserId         string    `json:"user_id"`
+	InitialBalance float64   `json:"initial_balance"`
+	CreatedAt      time.Time `json:"created_at"`
 }
 
 func NewAccount(balance float64, id string, name string, bank string) *Account {
