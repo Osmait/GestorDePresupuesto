617a80356ee985505c8118b84234bb3b1c5abb34
osmait <saulburgos6@gmail.com>
2024-01-18 11:00:30 -0400

    add color to constructor func

--- BackEnd/src/internals/domain/category/category.go
+++ BackEnd/src/internals/domain/category/category.go
@@ -11,10 +11,11 @@ type Category struct {
 	UserId    string    `json:"user_id"`
 }
 
-func NewCategory(id, name, icon string) *Category {
+func NewCategory(id, name, icon, color string) *Category {
 	return &Category{
-		Id:   id,
-		Name: name,
-		Icon: icon,
+		Id:    id,
+		Name:  name,
+		Icon:  icon,
+		Color: color,
 	}
 }
