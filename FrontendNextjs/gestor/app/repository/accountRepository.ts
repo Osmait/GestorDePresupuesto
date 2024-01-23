1f6b7b9653209b9a7ebcfe0b5da851f9acbcf6b6
osmait <saulburgos6@gmail.com>
2024-01-23 16:03:45 -0400

    add types to accountRepository

--- FrontendNextjs/gestor/app/repository/accountRepository.ts
+++ FrontendNextjs/gestor/app/repository/accountRepository.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 export class AccountRepository {
   private url = "http://127.0.0.1:8080";
 
-  async findAll() {
+  async findAll(): Promise<Account[]> {
     const token = cookies().get("x-token");
     const options = {
       headers: {
@@ -25,10 +25,11 @@ export class AccountRepository {
     bank: string,
     balance: number,
     user_id: string,
-  ) {
+  ): Promise<void> {
     const token = cookies().get("x-token");
     const options = {
       headers: {
+        method: "POST",
         "Content-Type": "application/json", // Especificamos que estamos enviando datos JSON
         Authorization: `Bearer ${token}`,
       },
@@ -37,6 +38,21 @@ export class AccountRepository {
     };
     try {
       await fetch(`${this.url}/account`, options);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async delete(id: string): Promise<void> {
+    const token = cookies().get("x-token");
+    const options = {
+      headers: {
+        method: "DELETE",
+        "Content-Type": "application/json", // Especificamos que estamos enviando datos JSON
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    try {
+      await fetch(`${this.url}/account/${id}`, options);
     } catch (error) {
       console.log(error);
     }
