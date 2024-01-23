c74a6506caf005e7a26be999965be149d3a83eb2
osmait <saulburgos6@gmail.com>
2024-01-23 16:04:50 -0400

    add transaction type

---
+++ FrontendNextjs/gestor/types/transaction.ts
@@ -0,0 +1,15 @@
+export enum TypeTransaction {
+  BILL,
+  INCOME,
+}
+export interface Transaction {
+  id: string;
+  name: string;
+  description: string;
+  amount: number;
+  type_transaction: TypeTransaction;
+  account_id: string;
+  category_id: string;
+  budget_id?: string;
+  created_at: Date;
+}
