fbe9cc986b763527944003d6ed7fc87d406fd6ef
osmait <saulburgos6@gmail.com>
2024-01-24 16:28:17 -0400

    remove comments

--- FrontendNextjs/gestor/app/app/page.tsx
+++ FrontendNextjs/gestor/app/app/page.tsx
@@ -27,24 +27,8 @@ export default function Home() {
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className=" md:flex  items-center md:justify-between space-y-2  ">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            {/* <div className="md:flex  items-center space-x-2 "> */}
-            {/*   <CalendarDateRangePicker className="mb-4 md:mb-0" /> */}
-            {/*   <Button>Download</Button> */}
-            {/* </div> */}
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
-            {/* <TabsList className="flex flex-col h-2/4 md:flex-row "> */}
-            {/*   <TabsTrigger value="overview">Overview</TabsTrigger> */}
-            {/*   <TabsTrigger value="analytics" disabled> */}
-            {/*     Analytics */}
-            {/*   </TabsTrigger> */}
-            {/*   <TabsTrigger value="reports" disabled> */}
-            {/*     Reports */}
-            {/*   </TabsTrigger> */}
-            {/*   <TabsTrigger value="notifications" disabled> */}
-            {/*     Notifications */}
-            {/*   </TabsTrigger> */}
-            {/* </TabsList> */}
             <TabsContent value="overview" className="space-y-4">
               <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
                 <Card>
